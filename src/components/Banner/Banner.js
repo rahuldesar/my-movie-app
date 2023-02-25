a33591e11980edd1da9a207ec12a73b28459d19f
Rahul Desar <desarrahul@gmail.com>
2023-02-25 19:46:34 +0545

    homepage css update

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -3,19 +3,27 @@ import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlay } from "@fortawesome/free-solid-svg-icons";
 
+import { useWindowDimensions } from "utils/getDimensions";
+
 import IMAGE_ROUTES from "constants/imageRoutes";
 import ROUTES from "constants/routes";
 
 // * Contains 3 element Image(Movie-Backdrop), Image-Overlay, and Movie-Detail
 const Banner = ({ bannerMovie: currentMovie }) => {
-  const movieDetails = `${ROUTES.DETAIL.BASE}/${currentMovie.id}`;
+  const { width } = useWindowDimensions();
+  const MOVIE_DETAILS = `${ROUTES.DETAIL.BASE}/${currentMovie.id}`;
+  const BANNER_IMAGE =
+    width > 700
+      ? `${IMAGE_ROUTES.ORIGINAL}${currentMovie.backdrop_path}`
+      : `${IMAGE_ROUTES.ORIGINAL}${currentMovie.poster_path}`;
+
   return (
     <>
       <div className="position-relative banner-wrapper">
         {/* Component 1 : Banner */}
         <div className="position-relative overflow-hidden bg-black banner-image-wrapper">
           <img
-            src={`${IMAGE_ROUTES.ORIGINAL}${currentMovie.backdrop_path}`}
+            src={BANNER_IMAGE}
             className="img-fluid d-block m-auto banner-image"
             alt=""
           />
@@ -32,12 +40,12 @@ const Banner = ({ bannerMovie: currentMovie }) => {
               </li>
             ))}
           </ul>
-          <Link to={movieDetails}>
+          <Link to={MOVIE_DETAILS}>
             <button className="btn btn-primary rounded-0 mt-2 mb-3 py-1 px-3">
               <FontAwesomeIcon icon={faPlay} />
             </button>
           </Link>
-          <p>{currentMovie.overview}</p>
+          <p className=" d-none d-md-block ">{currentMovie.overview}</p>
         </div>
       </div>
     </>
--- src/containers/Layout/Header.js
+++ src/containers/Layout/Header.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Link, NavLink } from "react-router-dom";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
+import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import InputGroup from "react-bootstrap/InputGroup";
 
 import { useTranslation } from "react-i18next";
 
@@ -15,25 +18,31 @@ import ROUTES from "constants/routes";
 import "assets/scss/components/header.scss";
 
 const Header = () => {
+  const [searchText, setSearchText] = useState("");
   const { t } = useTranslation();
   return (
     <header>
       <Navbar
         bg="black"
         expand="lg"
-        className="bg-opacity-25 navbar-wrapper navbar-dark w-100 border-bottom border-dark border-opacity-50 position-absolute"
+        fixed="top"
+        className="bg-opacity-75 navbar-wrapper navbar-dark w-100 border-bottom border-dark border-opacity-50 d-flex"
       >
         <Container>
+          <Navbar.Toggle
+            aria-controls="navbar-movie"
+            className="px-2 py-1 me-2"
+            data-toggle="collapse"
+          />
           <Navbar.Brand
             as={Link}
             to={ROUTES.HOME}
-            className="text-primary fw-bold"
+            className="text-primary fw-bold me-auto"
           >
             {t("label.title")}
           </Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbar-movie" data-toggle="collapse" />
-          <Navbar.Collapse id="navbar-movie">
-            <Nav className=" me-auto gap-lg-5 navbar-menu-items">
+          <Navbar.Collapse id="navbar-movie" className="order-2 order-lg-1">
+            <Nav className="me-auto ms-lg-3">
               <NavLink to={ROUTES.HOME} className="nav-link text-white">
                 Home
               </NavLink>
@@ -41,14 +50,19 @@ const Header = () => {
                 Movies
               </NavLink>
             </Nav>
-            <div>
-              <Link to={ROUTES.SEARCH.BASE}>
-                <span className=" bg-success p-2 rounded-circle text-white">
-                  <FontAwesomeIcon icon={faMagnifyingGlass} />
-                </span>
-              </Link>
-            </div>
           </Navbar.Collapse>
+          <div className="order-1 order-lg-2 position-relative">
+            <InputGroup size="sm" className="header-searchbox">
+              <Form.Control
+                placeholder="Search"
+                value={searchText}
+                onChange={(e) => setSearchText(e.target.value)}
+              />
+              <Button variant="outline-primary" id="">
+                <FontAwesomeIcon icon={faMagnifyingGlass} />
+              </Button>
+            </InputGroup>
+          </div>
         </Container>
       </Navbar>
     </header>
